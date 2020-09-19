70dca6336ce64ccceb76cc2fc26a24a4885377ad
liusiqian <liusiqian@kuaishou.com>
2020-09-19 19:51:06 +0800

    annotation--1

--- reflect/src/main/java/cn/liusiqian/reflectdemo/model/MyAnnotation.java
+++ reflect/src/main/java/cn/liusiqian/reflectdemo/model/MyAnnotation.java
@@ -1,4 +1,8 @@
 package cn.liusiqian.reflectdemo.model;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+@Target({ElementType.METHOD, ElementType.TYPE})
 public @interface MyAnnotation {
 }
